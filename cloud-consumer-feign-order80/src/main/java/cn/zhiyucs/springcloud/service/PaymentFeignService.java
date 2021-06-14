07183cb8363930aca881e64762fdb224f749623c
Zhiyu <542716863@qq.com>
2021-06-14 19:21:41 +0800

    June 14th, 2021
    June 14th, 2021

---
+++ cloud-consumer-feign-order80/src/main/java/cn/zhiyucs/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,18 @@
+package cn.zhiyucs.springcloud.service;
+
+import cn.zhiyucs.springcloud.entities.CommonResult;
+import cn.zhiyucs.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+}
